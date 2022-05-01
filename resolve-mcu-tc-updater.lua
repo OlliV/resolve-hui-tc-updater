bbc3a33a58b452d52d080cc3bc6ce55e20b7109b
allenrowand <32688520+allenrowand@users.noreply.github.com>
2022-04-30 23:08:52 -0700

    Add dots between time segements

--- resolve-mcu-tc-updater.lua
+++ resolve-mcu-tc-updater.lua
@@ -13,11 +13,11 @@ function tc_update ()
     end
     local d1 = (array [1]) + 48
     local d2 = (array [2]) + 48
-    local d3 = (array [3]) + 48
+    local d3 = (array [3]) + 112
     local d4 = (array [4]) + 48
-    local d5 = (array [5]) + 48
+    local d5 = (array [5]) + 112
     local d6 = (array [6]) + 48
-    local d7 = (array [7]) + 48
+    local d7 = (array [7]) + 112
     local d8 = (array [8]) + 48
     
     tcout = " cc 64 00" .. " cc 65 " .. d1 .. " cc 66 " .. d2 .. " cc 67 " .. d3 .. " cc 68 " .. d4 .. " cc 69 " .. d5 .. " cc 70 " .. d6 .. " cc 71 " .. d7 .. " cc 72 " .. d8
